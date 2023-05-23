4546fcd2a1c81aed28830eebcc0750f37bb50988
neerajnerlekar <neeraj.nerlekar@gmail.com>
2023-05-22 22:29:08 -0600

    make changes for heroku deployment try 2nd

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   app.enableCors();
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 3001);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
